6caa0c523b205bb3602e295ade54fd8fe6f04823
BitcoinMechanic <bitcoinmechanicca@protonmail.com>
2023-03-15 17:46:08 -0700

    add tentative electrs stuff

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -10,9 +10,12 @@ BTC_RPC_PROTOCOL=http
 BTC_RPC_TYPE="$(yq e '.bitcoind.type' /root/start9/config.yaml)"
 BTC_RPC_USER="$(yq e '.bitcoind.user' /root/start9/config.yaml)"
 BTC_RPC_PASSWORD="$(yq e '.bitcoind.password' /root/start9/config.yaml)"
-if [ "$BTC_RPC_TYPE" = "internal-proxy" ]; then
-	export BTC_RPC_HOST="btc-rpc-proxy.embassy"
-	echo "Running on Bitcoin Proxy..."
+if [ "$(yq e ".enable-electrs" /root/start9/config.yaml)" = "true" ]; then
+	sed -i 's/ELECTRUM_HOST:=127.0.0.1/ELECTRUM_HOST:=electrs.embassy/' start.sh
+	sed -i 's/ELECTRUM_PORT:=50002/ELECTRUM_PORT:=50001/' start.sh
+#if [ "$BTC_RPC_TYPE" = "internal-proxy" ]; then
+#	export BTC_RPC_HOST="btc-rpc-proxy.embassy"
+#	echo "Running on Bitcoin Proxy..."
 else
 	export BTC_RPC_HOST="bitcoind.embassy"
 	echo "Running on Bitcoin Core..."
