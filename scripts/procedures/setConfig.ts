eb577a33d962625abce1e381944cc1f676cf58f5
BitcoinMechanic <bitcoinmechanicca@protonmail.com>
2023-04-28 15:24:25 -0700

    setConfig add electrs

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -2,4 +2,24 @@ import {
   types as T,
   compat,
 } from "../deps.ts";
-export const setConfig: T.ExpectedExports.setConfig = compat.setConfig
+//export const setConfig: T.ExpectedExports.setConfig = compat.setConfig
+
+export const setConfig: T.ExpectedExports.setConfig = async (
+  effects: T.Effects,
+  input
+) => {
+  const newConfig = input as any;
+  const dependsOnElectrs: { [key: string]: string[] } = newConfig?.[
+    "enable-electrs"
+  ]
+    ? { electrs: ["synced"] }
+    : {};
+  const dependsOnBitcoind: { [key: string]: string[] } = newConfig?.txindex
+    ? { bitcoind: [] }
+    : {};
+    
+  return compat.setConfig(effects, newConfig, {
+    ...dependsOnElectrs,
+    ...dependsOnBitcoind,
+  });
+};
