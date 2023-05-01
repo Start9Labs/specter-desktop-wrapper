e597dd090178724e677779cbd03cc6d0abb6cbd9
Mariusz Kogen <k0gen@pm.me>
2023-05-02 00:41:12 +0200

    Initial setup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,26 +6,17 @@ _term() {
 }
 # Setting variables
 echo "Configuring Specter..."
-BTC_RPC_PROTOCOL=http
 BTC_RPC_TYPE="$(yq e '.bitcoind.type' /root/start9/config.yaml)"
 BTC_RPC_USER="$(yq e '.bitcoind.user' /root/start9/config.yaml)"
 BTC_RPC_PASSWORD="$(yq e '.bitcoind.password' /root/start9/config.yaml)"
+
 if [ "$BTC_RPC_TYPE" = "internal-proxy" ]; then
-	export BTC_RPC_HOST="btc-rpc-proxy.embassy"
-	echo "Running on Bitcoin Proxy..."
-else
-
-export BTC_RPC_HOST="bitcoind.embassy"
-echo "Running on Bitcoin Core..."
+  jq '.active_node_alias = "Bitcoin Proxy"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
+elif [ "$BTC_RPC_TYPE" = "electrs" ]; then
+  jq '.active_node_alias = "spectrum_node"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
+elif [ "$BTC_RPC_TYPE" = "internal" ]; then
+  jq '.active_node_alias = "Bitcoin"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
 fi
-export ELECTRUM_HOST="electrs.embassy"
-echo $ELECTRUM_HOST
-export ELECTRUM_PORT=50001
-echo $ELECTRUM_PORT
-echo "Running on Electrs on port 50001"
-
-echo "Starting Specter..."
-export BTC_RPC_PORT=8332
 
 python3 -m cryptoadvance.specter server --host 0.0.0.0 &
 specter_process=$!
