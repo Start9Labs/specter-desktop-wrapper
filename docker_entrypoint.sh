61fb57d66f6a5b012d88ae086f8adde9c3598eb5
BitcoinMechanic <bitcoinmechanicca@protonmail.com>
2023-05-23 13:20:42 -0700

    fix bitcoin auto populate

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -18,46 +18,12 @@ if [ ! -f /root/.specter/config.json ]; then
     echo "Starting specter server for 5 seconds..."
     python3 -m cryptoadvance.specter server --host 0.0.0.0 &
     pid=$!  # Save process ID
-    sleep 5  # Wait for 5 seconds
+    sleep 15  # Wait for 5 seconds
     kill $pid  # Kill the process
     echo "Specter server killed"
 fi
 
-if [ "$BTC_RPC_TYPE" = "internal-proxy" ]; then
-  jq '.active_node_alias = "bitcoin_proxy"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
-  cat <<EOF > /root/.specter/nodes/bitcoin_proxy.json
-{
-    "python_class": "cryptoadvance.specter.node.Node",
-    "fullpath": "/root/.specter/nodes/bitcoin_proxy.json",
-    "name": "Bitcoin Proxy",
-    "alias": "bitcoin_proxy",
-    "autodetect": false,
-    "datadir": "",
-    "user": "$BTC_RPC_USER",
-    "password": "$BTC_RPC_PASSWORD",
-    "port": "8332",
-    "host": "btc-rpc-proxy.embassy",
-    "protocol": "http",
-    "node_type": "BTC"
-}
-EOF
-rm -f /root/.specter/nodes/spectrum_node.json
-
-elif [ "$BTC_RPC_TYPE" = "electrs" ]; then
-  jq '.active_node_alias = "spectrum_node"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
-  cat <<EOF > /root/.specter/nodes/spectrum_node.json
-{
-    "python_class": "cryptoadvance.specterext.spectrum.spectrum_node.SpectrumNode",
-    "fullpath": "/root/.specter/nodes/spectrum_node.json",
-    "name": "Spectrum Node",
-    "alias": "spectrum_node",
-    "host": "electrs.embassy",
-    "port": 50001,
-    "ssl": false
-}
-EOF
-
-elif [ "$BTC_RPC_TYPE" = "internal" ]; then
+if [ "$BTC_RPC_TYPE" = "internal" ]; then
   jq '.active_node_alias = "bitcoin_core"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
   cat <<EOF > /root/.specter/nodes/bitcoin_core.json
 {
@@ -77,6 +43,64 @@ elif [ "$BTC_RPC_TYPE" = "internal" ]; then
 EOF
 
 rm -f /root/.specter/nodes/spectrum_node.json
+
+elif [ "$BTC_RPC_TYPE" = "internal-proxy" ]; then
+  jq '.active_node_alias = "bitcoin_proxy"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
+  cat <<EOF > /root/.specter/nodes/bitcoin_proxy.json
+{
+    "python_class": "cryptoadvance.specter.node.Node",
+    "fullpath": "/root/.specter/nodes/bitcoin_proxy.json",
+    "name": "Bitcoin Proxy",
+    "alias": "bitcoin_proxy",
+    "autodetect": false,
+    "datadir": "",
+    "user": "$BTC_RPC_USER",
+    "password": "$BTC_RPC_PASSWORD",
+    "port": "8332",
+    "host": "btc-rpc-proxy.embassy",
+    "protocol": "http",
+    "node_type": "BTC"
+}
+EOF
+
+rm -f /root/.specter/nodes/spectrum_node.json
+
+elif [ "$BTC_RPC_TYPE" = "electrs" ]; then
+  jq '.active_node_alias = "spectrum_node"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
+  cat <<EOF > /root/.specter/nodes/spectrum_node.json
+{
+    "python_class": "cryptoadvance.specterext.spectrum.spectrum_node.SpectrumNode",
+    "fullpath": "/root/.specter/nodes/spectrum_node.json",
+    "name": "Spectrum Node",
+    "alias": "spectrum_node",
+    "host": "electrs.embassy",
+    "port": 50001,
+    "ssl": false
+}
+EOF
+
+rm -f /root/.specter/nodes/bitcoin_node.json
+
+# elif [ "$BTC_RPC_TYPE" = "internal" ]; then
+#   jq '.active_node_alias = "bitcoin_core"' /root/.specter/config.json > /root/.specter/config.tmp && mv /root/.specter/config.tmp /root/.specter/config.json
+#   cat <<EOF > /root/.specter/nodes/bitcoin_core.json
+# {
+#     "python_class": "cryptoadvance.specter.node.Node",
+#     "fullpath": "/root/.specter/nodes/bitcoin_core.json",
+#     "name": "Bitcoin Core",
+#     "alias": "bitcoin_core",
+#     "autodetect": false,
+#     "datadir": "",
+#     "user": "$BTC_RPC_USER",
+#     "password": "$BTC_RPC_PASSWORD",
+#     "port": "8332",
+#     "host": "bitcoind.embassy",
+#     "protocol": "http",
+#     "node_type": "BTC"
+# }
+# EOF
+
+# rm -f /root/.specter/nodes/spectrum_node.json
 
 fi
 
