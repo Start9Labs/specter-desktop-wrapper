1e48637de100e5700f6e6f47243921d37ffc6e6d
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-07-03 18:01:59 -0400

    add migration for deprecation of proxy

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,30 @@
-import { compat, types as T } from "../deps.ts";
+import { compat, types as T, matches } from "../deps.ts";
+
+const { shape, string } = matches
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping( {}, "1.13.1" );
+  .fromMapping(
+    {
+    "2.0.2.2": {
+      up: compat.migrations.updateConfig((config) => {
+        if (Object.keys(config).length === 0) {
+          // service was never configured
+          return config
+        }
+        const matchConfig = shape({
+        bitcoind: shape({
+          type: string
+        })
+      })
+      if (!matchConfig.test(config)) {
+        throw `Incorrect shape for config: ${matchConfig.errorMessage(config)}`
+      }
+        if (config.bitcoind.type === 'internal-proxy') {
+          config.bitcoind.type = 'internal'
+        }
+        return config
+      }, true, { version: "2.0.2.2", type: "up" }),
+      down: compat.migrations.updateConfig(_ => { throw new Error("Downgrade unavailable") }, true, { version: "2.0.2.2", type: "down" })
+    }
+  }
+  , "2.0.2.2" );
